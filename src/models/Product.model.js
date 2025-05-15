2383ec021de308f2b9f70a4c1924b20e3d6988f5
galeed gutierrez <galeedeveloper@gmail.com>
2025-05-15 02:06:44 -0300

    feat: add Product model definition with properties for inventory management

---
+++ src/models/Product.model.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {object} Product - Represents a product in the inventory.
+ * @property {number} id - Unique identifier of the product.
+ * @property {string} name - Name of the product.
+ * @property {string} description - Description of the product.
+ * @property {number} stock - Stock quantity of the product.
+ * @property {number} price - Price of the product.
+ * @property {number} discount - Discount percentage on the product.
+ * @property {number} priceWithDiscount - Price after applying the discount.
+ */
+
+// Empty export to ensure this file is treated as a module
+export {};
