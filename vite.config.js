13931dfccebe8d4572b11c432af1f5de5256be1d
galeed gutierrez <galeedeveloper@gmail.com>
2025-05-16 01:29:05 -0300

    feat: add lightningcss for improved CSS processing and minification

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import react from '@vitejs/plugin-react-swc';
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
 import { defineConfig } from 'vite';
 // @ts-ignore
 import jsconfigPaths from 'vite-jsconfig-paths';
@@ -7,4 +9,13 @@ import jsconfigPaths from 'vite-jsconfig-paths';
 export default defineConfig({
 	// @ts-ignore
 	plugins: [react(), jsconfigPaths()],
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			targets: browserslistToTargets(browserslist('>= 0.25%')),
+		},
+	},
+	build: {
+		cssMinify: 'lightningcss',
+	},
 });
