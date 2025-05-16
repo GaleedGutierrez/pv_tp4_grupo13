7a2c98bfe074c5cf0031adaa25550db144eee098
galeed gutierrez <galeedeveloper@gmail.com>
2025-05-16 01:29:31 -0300

    refactor: improve root element rendering logic in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import './index.css';
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+const ROOT = document.getElementById('root');
+
+if (ROOT instanceof HTMLDivElement) {
+	createRoot(ROOT).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+}
