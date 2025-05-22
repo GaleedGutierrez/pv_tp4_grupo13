15a91a74811671bbdc1815c2ea0b8fe884bbe6ea
galeed gutierrez <galeedeveloper@gmail.com>
2025-05-22 20:17:18 -0300

    feat: add ProductItem styles and update Product model documentation

--- src/models/Product.model.js
+++ src/models/Product.model.js
@@ -1,11 +1,11 @@
 /**
  * @typedef {object} Product - Represents a product in the inventory.
- * @property {number} id - Unique identifier of the product.
+ * @property {string} id - Unique identifier of the product.
  * @property {string} description - Description of the product.
- * @property {number} stock - Stock quantity of the product.
- * @property {number} price - Price of the product.
- * @property {number} [discount] - Discount percentage on the product.
- * @property {number} [priceWithDiscount] - Price after applying the discount.
+ * @property {string} stock - Stock quantity of the product.
+ * @property {string} price - Price of the product.
+ * @property {string} [discount] - Discount percentage on the product.
+ * @property {string} [priceWithDiscount] - Price after applying the discount.
  */
 
 // Empty export to ensure this file is treated as a module
